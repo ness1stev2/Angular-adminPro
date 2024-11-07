a8a0c37d6506fbdb87596e2734b7aa14a447127f
ness1stev2 <ne.s.tor-10@hotmail.com>
2024-11-07 15:14:41 -0600

    implementacion de rutas secundarias

---
+++ src/app/shared/breadcrums/breadcrums.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-breadcrums',
+  templateUrl: './breadcrums.component.html',
+  styles: ``
+})
+export class BreadcrumsComponent {
+
+}
